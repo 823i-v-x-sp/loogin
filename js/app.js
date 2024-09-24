3dae925e07ea0c7b52eef5c30856144e780ff21a
823i-v-x-sp <os8233665@gmail.com>
2024-09-24 17:42:28 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/6ddd59ed-e06a-4459-bdf7-71a4f402e567', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Telefono": formulario.usuario.value
+        
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
